4da1ec041898b8462500d8879122708a9f34eb8b
황주완 <101560702+HamTory06@users.noreply.github.com>
2022-10-07 18:43:27 +0900

    레트로핏 오류

--- app/src/main/java/com/example/popcat/MainActivity.kt
+++ app/src/main/java/com/example/popcat/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.widget.ImageView
+import com.example.popcat.PHP.Companion.pop
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,23 +15,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val request = PHP.pop.POP(id)
-        request.enqueue(object: Callback<POP>{
-            override fun onResponse(call: Call<POP>, response: Response<POP>) { todo.value = response.body()
-                Log.d("RESPONSE", "Response: ${response.code()}")
-            }
-            override fun onFailure(call: Call<POP>, t: Throwable) {
-
-            }
-        })
     }
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         val main = findViewById<ImageView>(R.id.main)
+        val request = PHP.pop
         when (event?.action) {
             MotionEvent.ACTION_DOWN -> {
                 main.setImageResource(R.drawable.opencat)
                 Log.d("상태", "클릭")
-
+                request.GetCounts().enqueue(object: Callback<POP>{
+                    override fun onResponse(call: Call<POP>, response: Response<POP>) {
+                        if(response.isSuccessful){
+                        }else{
+                            Log.d("YMC", "onResponse 실패")
+                        }
+                    }
+                    override fun onFailure(call: Call<POP>, t: Throwable) {
+                        Log.d("오류","오류(서버)")
+                    }
+                })
             }
             MotionEvent.ACTION_UP -> {
                 main.setImageResource(R.drawable.closecat)
