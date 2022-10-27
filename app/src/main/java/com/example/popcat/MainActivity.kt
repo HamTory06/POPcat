12e714970c251131b65bf637725cf3eb45718f9d
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2022-10-27 12:49:45 +0900

    터치하면 소리나게함(구현 완료)

--- app/src/main/java/com/example/popcat/MainActivity.kt
+++ app/src/main/java/com/example/popcat/MainActivity.kt
@@ -34,7 +34,6 @@ class MainActivity : AppCompatActivity() {
                 mediaplayer!!.setOnCompletionListener {
                     it.stop()
                 }
-
                 Log.d("상태", "클릭")
             }
             MotionEvent.ACTION_UP -> {
