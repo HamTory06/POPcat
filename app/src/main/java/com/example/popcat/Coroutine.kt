cb058e9204ba849d60be4d90717794cf66f6bb7d
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2022-11-07 21:36:34 +0900

    popcat 코루틴 사용중(구현 덜함)

--- app/src/main/java/com/example/popcat/Coroutine.kt
+++ app/src/main/java/com/example/popcat/Coroutine.kt
@@ -2,11 +2,18 @@ package com.example.popcat
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent.ACTION_DOWN
+import android.view.MotionEvent
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import okhttp3.Dispatcher
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Coroutine : AppCompatActivity(){
 
@@ -17,19 +24,21 @@ class Coroutine : AppCompatActivity(){
         setContentView(R.layout.activity_main)
     }
 
-    fun a(){
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
         CoroutineScope(Dispatchers.Main).async {
-            val sound = CoroutineScope(Dispatchers.Default).async {
-                mediaplayer = MediaPlayer.create(this@Coroutine, R.raw.popcat_sound)
-                mediaplayer!!.start() // 소리 재생
-                mediaplayer!!.setOnCompletionListener {
-                    it.stop()
-                }
-            }.await()
-            val
-        }//메인 스레드
-    }
-    fun Retrofit(){
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    CoroutineScope(Dispatchers.Default).async {
 
+                    }
+                }
+                MotionEvent.ACTION_UP -> {
+                    CoroutineScope(Dispatchers.Default).async {
+
+                    }
+                }
+            }
+        }
+        return super.onTouchEvent(event)
     }
 }
